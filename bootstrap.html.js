bfb78be6504d3b8250cd2212875975ad5f59293e
marchuanv <Marchuanv@gmail.com>
2018-08-20 21:28:23 +0200

    Update bootstrap.html.js

--- bootstrap.html.js
+++ bootstrap.html.js
@@ -1,5 +1,5 @@
 const utils = new Utils();
-utils.loadScript(document,"https://cdnjs.cloudflare.com/ajax/libs/three.js/95/three.min.js",()=>{
+utils.loadClientScript(document,"https://cdnjs.cloudflare.com/ajax/libs/three.js/95/three.min.js",()=>{
   const frustumSize = 1000;
   const aspect = window.innerWidth / window.innerHeight;
   const camera = new THREE.OrthographicCamera( frustumSize * aspect / - 2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, 1, 2000 );
