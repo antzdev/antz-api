079cbdc78fcf592f7c5671b56ee5309b5bee70ce
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2018-08-27 15:35:01 +0200

    fix

--- bootstrap.js
+++ bootstrap.js
@@ -1,18 +1,13 @@
 const utils = new Utils();
 utils.loadClientScript(document,"https://cdnjs.cloudflare.com/ajax/libs/three.js/95/three.min.js",()=>{
-	
 	const sceneFactory=new SceneFactory();
-	const scene = sceneFactory.create("scenetest",[{
-		name: "explosion",
-		type: "Sprite"
-	}]);
-	
-	document.body.appendChild(scene.renderer.domElement );
-	window.addEventListener('resize',()=>{
-		scene.camera.aspect=(window.innerWidth/window.innerHeight);
-		scene.camera.updateProjectionMatrix();
-		scene.renderer.setSize( window.innerWidth, window.innerHeight );
-	},false);
-
-	scene.render();
+	sceneFactory.create("scenetest",[{name: "explosion", type: "Sprite"}]).then((scene)=>{
+		document.body.appendChild(scene.renderer.domElement );
+		window.addEventListener('resize',()=>{
+			scene.camera.aspect=(window.innerWidth/window.innerHeight);
+			scene.camera.updateProjectionMatrix();
+			scene.renderer.setSize( window.innerWidth, window.innerHeight );
+		},false);
+		scene.render();
+	});
 });
