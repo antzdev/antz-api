1d9eafd6bced3afb2bf9dcbca965aedef0c8ca5b
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2018-08-27 16:27:49 +0200

    fix

--- bootstrap.js
+++ bootstrap.js
@@ -1,7 +1,10 @@
 const utils = new Utils();
 utils.loadClientScript(document,"https://cdnjs.cloudflare.com/ajax/libs/three.js/95/three.min.js",()=>{
 	const sceneFactory=new SceneFactory();
-	sceneFactory.create("scenetest").then((scene)=>{
+	sceneFactory.create({
+		name: "example",
+		entities: [{name: "example",type:"png"}]
+	}).then((scene)=>{
 		document.body.appendChild(scene.renderer.domElement );
 		window.addEventListener('resize',()=>{
 			scene.camera.aspect=(window.innerWidth/window.innerHeight);
