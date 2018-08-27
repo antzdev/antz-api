ac76120010b23583e9a5c9a40f5ed44a10969e49
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2018-08-27 13:42:24 +0200

    example

--- bootstrap.js
+++ bootstrap.js
@@ -1,7 +1,15 @@
 const utils = new Utils();
 utils.loadClientScript(document,"https://cdnjs.cloudflare.com/ajax/libs/three.js/95/three.min.js",()=>{
-  const frustumSize = 1000;
-  const aspect = window.innerWidth / window.innerHeight;
-  const camera = new THREE.OrthographicCamera( frustumSize * aspect / - 2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, 1, 2000 );
-  console.log("camera created");
+	
+	const sceneFactory=new SceneFactory();
+	const scene = sceneFactory.create("Box");
+	
+	document.body.appendChild(scene.renderer.domElement );
+	window.addEventListener('resize',()=>{
+		scene.camera.aspect=(window.innerWidth/window.innerHeight);
+		scene.camera.updateProjectionMatrix();
+		scene.renderer.setSize( window.innerWidth, window.innerHeight );
+	},false);
+
+	scene.render();
 });
