75d3c1f20569c26acca06c9debf852830d7fa8ff
marchuanv <marchuanv@gmail.com>
2018-08-29 20:23:32 +0200

    revert

--- bootstrap.js
+++ bootstrap.js
@@ -1,11 +1,14 @@
 const utils = new Utils();
 utils.loadClientScript(document,"https://cdnjs.cloudflare.com/ajax/libs/three.js/95/three.min.js",()=>{
-	const gameFactory=new GameFactory();
-	gameFactory.create().then((game)=>{
-		document.body.appendChild(game.domElement);
+	const sceneFactory=new SceneFactory({
+		name: "example",
+		entities: [{name: "example",type:"png"}]
+	});
+	sceneFactory.create().then((scene)=>{
+		document.body.appendChild(scene.domElement );
 		window.addEventListener('resize',()=>{
-			game.reset();
+			scene.reset();
 		},false);
-		game.render();
+		scene.render();
 	});
 });
