f81258f459da095098716ec90f08519d42bff431
MarchuanVan Der Merwe <marchuanv@gmail.com>
2018-08-29 17:36:43 +0200

    Update bootstrap.js - Codepanel

--- bootstrap.js
+++ bootstrap.js
@@ -2,7 +2,7 @@ const utils = new Utils();
 utils.loadClientScript(document,"https://cdnjs.cloudflare.com/ajax/libs/three.js/95/three.min.js",()=>{
 	const gameFactory=new GameFactory();
 	gameFactory.create().then((game)=>{
-		document.body.appendChild(game.domElement );
+		document.body.appendChild(game.domElement);
 		window.addEventListener('resize',()=>{
 			game.reset();
 		},false);
